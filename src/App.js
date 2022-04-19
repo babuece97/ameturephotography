817a57a809992df09b2d374b8d0d6fa81366b884
babuece97 <babuece97@yahoo.com.com>
2022-04-19 02:31:07 +0200

    blog completed

--- src/App.js
+++ src/App.js
@@ -17,16 +17,17 @@ function App() {
     <div>
       <Navbar></Navbar>
       <Banner></Banner>
-      <Footer></Footer>
+      
       <Routes>
           <Route path="/" element={<Home></Home>}/>
           <Route path="home" element={<Home></Home>} />
           <Route path="services" element={<Services></Services>}/>
           <Route path="blog" element={<Blog> </Blog>}/>
-          <Route path="about me" element={<About></About>} />
+          <Route path="aboutMe" element={<About></About>} />
           <Route path="registration" element={<Registration> </Registration> } />
           <Route path="login" element={<Login></Login>} />
         </Routes>
+        <Footer></Footer>
     </div>
   );
 }
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,26 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            My blog
+            <h2> Difference between authentication & authorization</h2>
+            <p> Are you right person to do the specific job is called authetication and  the area of tasks for specific person is known as authorization. For example, if you right to enter the office, the we call it authetication and  eventually you have entered into the office and  certainly your tasks are being  somehow limited ,then we call it authorization </p>
+            <h2> Why are you using firebase? What other options do you have to implement authentication?</h2>
+            <p> Since now-a-days security is the big issue to use the internet, specially user ID, password, personal data etc. Therefore the main aims to is   secure authentication systems easy, while improving the sign-in and onboarding experience for end users. It provides an end-to-end identity solution, supporting email and password accounts </p>
+            <p> There are lots of opitions to implement for authentication.
+                <li> Multi-Factor Authentication</li>
+                <li> Single-Factor/Primary Authentication</li>
+                <li>Biometric authentication</li>
+                <li> Token-based authentication</li>
+                etc
+            </p>
+            <h2>What other services does firebase provide other than authentication?</h2>
+            <p> Other than authentication , we can do lots of things with firebase. fx
+                <li> Cloud Storage</li>
+                <li> Hosting</li>
+                <li> Cloud Functions</li>
+                <li> Google Analytics</li>
+                <li> Dynamic Links</li>
+                etc
+            </p>
         </div>
     );
 };
